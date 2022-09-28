51c77c63009bac86499fba822a01de809fa30009
David Chica <david.chikhb@gmail.com>
2022-09-28 17:36:25 -0500

    anadido boton para subir, e inclusion de la libreria fontawesome

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//CODIGO PARA EL CAROUSEL
 const carousel = document.querySelector("#carousel");
 let sliderSection = document.querySelectorAll(".slider-div");
 let sliderLast = sliderSection[sliderSection.length -1];
@@ -42,3 +43,29 @@ leftBtn.addEventListener('click', function(){
 setInterval(function(){
     moveRight();
 }, 5000);
+            //FIN DEL CODIGO DE CAROUSEL
+
+
+
+//CODIGO PARA EL BOTON SUBIR
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector(".go-top-container")
+        .classList.add("show");
+    }else{
+        document.querySelector(".go-top-container")
+        .classList.remove("show");
+    }
+}
+
+document.querySelector(".go-top-container")
+.addEventListener("click", () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+// console.log(document.documentElement.scrollTop)
+// utilizar esta linea para ver cuantos pixeles ha bajado
+
+//FIN DEL CODIGO PARA EL BOTON SUBIR
